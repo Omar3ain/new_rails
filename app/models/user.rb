3fae5d294908f7b2f2a45e2efe7665bda412402e
Soad Ahmed <soadahmed1498@gmail.com>
2024-05-29 20:59:01 +0300

    add presence validation on all users fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    validates :name, presence: true
+    validates :DOB, presence: true
+    validates :email, presence: true
+    validates :phone_number, presence: true
 end
