bcbd66860d6398e6abf1ca12f658a06b9f58e730
Soad Ahmed <soadahmed1498@gmail.com>
2024-05-29 21:02:47 +0300

    add validation on email structure

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     validates :name, presence: true
     validates :DOB, presence: true
-    validates :email, presence: true
+    validates :email, presence: true,format: { with: URI::MailTo::EMAIL_REGEXP } 
     validates :phone_number, presence: true
 end
