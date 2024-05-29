2c7bf59af87767fee8c51caed4b9690df3ac0673
Soad Ahmed <soadahmed1498@gmail.com>
2024-05-29 13:46:11 +0300

    adding content field to posts migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_28_105249) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_29_104324) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "content"
   end
 
 end
