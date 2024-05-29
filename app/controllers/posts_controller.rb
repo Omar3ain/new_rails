e904ad19da8d738a1815cd4047e24f55dfcf2086
Soad Ahmed <soadahmed1498@gmail.com>
2024-05-29 16:15:25 +0300

    create edit/update methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,4 +15,23 @@ class PostsController < ApplicationController
             render :new , status:422
         end        
     end
+
+    def show
+        @post=Post.find(params[:id])
+    end   
+    
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def def update
+        @post = Post.find(params[:id])
+        if @post.update({title: params[:post][:title],content:params[:post][:content]})
+           redirect_to post_url(@post)
+        else
+           render :new , status:422
+        end
+    end
+        
+    end
 end    
