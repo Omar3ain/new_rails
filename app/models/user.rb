4bce92bba432a4be634550718348f4b2a1957f8c
Gehad Marawan <gehadmarawan7@gmail.com>
2024-05-30 02:12:28 +0300

    validate email and uniqness

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   validates :DOB, presence: true
   validates :email, presence: true
   validates :phone_number, presence: true
-
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
+  validates :email, uniqueness: true
 end
