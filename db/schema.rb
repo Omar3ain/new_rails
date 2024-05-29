d7330e0c9d2a02d77aac54028361221c6dd06990
Gehad Marawan <gehadmarawan7@gmail.com>
2024-05-30 02:11:11 +0300

    create user and validate it

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_29_131703) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_29_230802) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "content"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.date "DOB"
+    t.string "email"
+    t.string "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
