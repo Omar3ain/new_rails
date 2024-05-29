66d4bbe6df5a99b998a0c95a6572c47951a30bc3
Toqa <toqaasattar@gmail.com>
2024-05-30 00:17:29 +0300

    posts controller and routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,46 @@
 class PostsController < ApplicationController
     def index 
+        @posts=Post.all
+    end
+
+    def new 
+        @post=Post.new
         
+    end
+
+    def edit 
+        @post=Post.find(params[:id])
+    end
+
+    def update 
+        @post=Post.find(params[:id])
+        if @post.update({title:params[:post][:title], content:params[:post][:content]})
+            redirect_to post_url(@post)
+        else
+            render :edit, status: 422
+        end 
+    end
+
+    def destroy 
+        @post =Post.find(params[:id])
+        @post.destroy!
+        redirect_to post_url
+    end
+
+    def show 
+        @post=Post.find(params[:id])
+        
+    end
+
+    def create
+
+        @post=Post.new(title:params[:post][:title],content:[:post][:content])
+
+        if @post.save
+            redirect_to post_url(@post)
+        else
+            render :new
+        end
+
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+    validate :title, presence: true
+    validate :content, presence: true
 end
