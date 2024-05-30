9abc74531ec697c51ae7a0f17c9f7e1a7eeef4af
Ahmed Kamal mahmoud <eng.ahmedkamal357@gmail.com>
2024-05-30 14:23:53 +0300

    create CRUD for post resource

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,45 @@
 class PostsController < ApplicationController
     def index
+        # @post = "post 1"
+        @posts = Post.all
     end 
+
+    def show
+        @post = Post.find(params[:id])
+    end
+    def new
+        @post = Post.new
+    end
+    def create
+        @post = Post.new({title:params[:post][:title], content:params[:post][:content]})
+        
+        if @post.save
+            redirect_to posts_url
+        else
+            render :new, status:422
+        end
+    end
+    def edit 
+        @post =Post.find(params[:id])
+    end
+    def update
+        @post = Post.find(params[:id])
+        if @post.update({title:params[:post][:title],content:params[:post][:content]})
+            redirect_to posts_url
+
+        else    
+            render :edit, status: 422
+        end
+    end
+    def destroy
+            @post = Post.find(params[:id])
+            @post.destroy
+            respond_to do |format|
+                format.html { redirect_to posts_url, notice: "posts has been destroyed." }
+              end
+    end
+
+
+    
+
 end
