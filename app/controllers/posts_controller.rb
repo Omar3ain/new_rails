81b275b6baf93bd258acbc0ac75c8d607a7fa568
saraalyan <alyansara817@gmail.com>
2024-05-30 13:57:03 +0300

    create index for all and new and create and show method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,21 @@
-class PostsController < ActionController::Base
+class PostsController < ApplicationController
     def index
+        @posts=Post.all
     end
+
+    def new
+        @post=Post.new
+    end  
+    
+    def create
+        @post=Post.new({title: params[:post][:title],content:params[:post][:content]})
+        if @post.save
+            redirect_to post_url(@post)
+        else
+            render :new , status:422
+        end        
+    end
+    def show
+        @post=Post.find(params[:id])
+    end   
 end
