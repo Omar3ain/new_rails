8f05f9c1ab12ad2781f819cfca288af7863c100b
Yomna khaled <yomnakhaled2001@gmail.com>
2024-06-01 23:26:04 +0300

    add all post routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # get 'users/index'
+  # get "users",to: "users#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -7,5 +9,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get "posts",to: "posts#index"
+  # get "posts",to: "posts#index"
+  resources :posts 
 end
