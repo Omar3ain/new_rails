f8709afba94b1ad0399716a4c732450e003071b2
Yomna khaled <yomnakhaled2001@gmail.com>
2024-06-02 00:00:06 +0300

    make user model and it's migration and add views of user model

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+    EMAIL_REGEXP=/\A[a-zA-Z0-9]+@[a-zA-Z]+(\.[a-zA-Z]{2,})+\z/
+    validates :name, presence: true
+    validates :DOB, presence: true
+    validates :email, presence: true,uniqueness: true,format: { with: EMAIL_REGEXP } 
+    validates :phone_number, presence: true
+
+end
