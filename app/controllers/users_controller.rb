e7c2ed36c4c0d5e5ccc08ed93172fab8ae8e538b
Toqa <toqaasattar@gmail.com>
2024-06-02 13:54:09 +0300

    Editted  user controllers file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,45 @@
 class UsersController < ApplicationController
+    def index 
+        @users=User.all
+    end
+
+    def new 
+        @user=User.new
+        
+    end
+
+    def edit 
+        @user=User.find(params[:id])
+    end
+
+    def update 
+        @user=User.find(params[:id])
+        if @user.update({name: params[:user][:name], DOB: params[:user][:DOB], email: params[:user][:email], phone_number: params[:user][:phone_nubmer]})
+            redirect_to user_url(@user)
+        else
+            render :edit, status: 422
+        end 
+    end
+
+    def destroy 
+        @user =User.find(params[:id])
+        @user.destroy!
+        redirect_to user_url
+    end
+
+    def show 
+        @user=User.find(params[:id])
+        
+    end
+
+    def create
+
+        @user = User.new(name: params[:user][:name], DOB: params[:user][:DOB], email: params[:user][:email], phone_number: params[:user][:phone_nubmer])
+        if @user.save
+            redirect_to user_url(@user)
+        else
+            render :new
+        end
+
+    end
 end
