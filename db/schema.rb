3d741754fddce6d0f568412f706e6250abf39e34
Ola Hamdy <olahamdy@localhost.localdomain>
2024-06-06 13:48:11 +0300

    Create a Creator Association in the Author Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_06_094248) do
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.integer "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_posts_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
+ActiveRecord::Schema[7.1].define(version: 2024_06_06_103404) do
+  create_table "authors", force: :cascade do |t|
     t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "posts", "users"
 end
